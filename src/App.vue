3c6afc372af187081f1d74d094d04247e2873cb6
李美丽 <304489471@qq.com>
2019-03-08 20:16:53 +0800

    Tab组件上下联动

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
     <v-header :seller="seller"></v-header>
+    <div class="tab-wrapper">
+      <tab :tabs="tabs" :initialIndex="1"></tab>
+    </div>
   </div>
 </template>
 <script>
 
 import VHeader from 'components/v-header/v-header'
+import Tab from 'components/tab/tab'
 import { getSeller } from 'api'
+import Goods from 'components/goods/goods'
+import Ratings from 'components/ratings/ratings'
+import Seller from 'components/seller/seller'
 
 export default {
   name: 'app',
@@ -17,9 +24,37 @@ export default {
   },
   components: {
     VHeader,
+    Tab
   },
   created() {
    this._getSeller()
+  },
+  computed: {
+    tabs() {
+      return [
+        {
+          label: '商品',
+          component: Goods,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: '评论',
+          component: Ratings,
+          data: {
+            seller: this.seller
+          }
+        },
+        {
+          label: '商家',
+          component: Seller,
+          data: {
+            seller: this.seller
+          }
+        }
+      ]
+    }
   },
   methods: {
     _getSeller() {
@@ -31,4 +66,13 @@ export default {
 }
 </script>
 <style lang="stylus">
+  @import "~common/stylus/variable"
+
+  #app
+    .tab-wrapper
+      position: fixed
+      top: 136px
+      left: 0
+      right: 0
+      bottom: 0
 </style>
---
+++ src/components/tab/tab.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="tab">
+    <cube-tab-bar
+      :showSlider=true
+      :use-transition=false
+      v-model="selectedLabel"
+      :data="tabs"
+      ref="tabBar"
+      class="border-bottom-1px"
+    >
+    </cube-tab-bar>
+    <div class="slide-wrapper">
+      <cube-slide
+        :loop=false
+        :auto-play=false
+        :show-dots=false
+        :initial-index="index"
+        :options="slideOptions"
+        ref="slide"
+        @change="onChange"
+        @scroll="onScroll"
+      >
+        <cube-slide-item>
+          <goods></goods>
+        </cube-slide-item>
+        <cube-slide-item>
+          <ratings></ratings>
+        </cube-slide-item>
+        <cube-slide-item>
+          <seller></seller>
+        </cube-slide-item>
+      </cube-slide>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Goods from 'components/goods/goods'
+  import Seller from 'components/seller/seller'
+  import Ratings from 'components/ratings/ratings'
+
+  export default {
+    name: 'tab',
+//    props: {
+//      tabs: {
+//        type: Array,
+//        default() {
+//          return []
+//        }
+//      },
+//      initialIndex: {
+//        type: Number,
+//        default: 0
+//      }
+//    },
+    data() {
+      return {
+        index: 0,
+        tabs: [{
+          label: '商品',
+        }, {
+          label: '评价',
+        }, {
+          label: '商家',
+        }],
+        slideOptions: {
+          listenScroll: true,
+          probeType: 3,
+          directionLockThreshold: 0
+        },
+
+      }
+    },
+    computed: {
+      selectedLabel: {
+        get() {
+          return this.tabs[this.index].label
+        },
+        set(newVal) {
+          this.index = this.tabs.findIndex((value) => {
+            return value.label === newVal
+          })
+        }
+      }
+    },
+    methods: {
+      onScroll(pos) {
+        const tabBarWidth = this.$refs.tabBar.$el.clientWidth
+        const slideWidth = this.$refs.slide.slide.scrollerWidth
+        const transform = -pos.x / slideWidth * tabBarWidth
+        this.$refs.tabBar.setSliderTransform(transform)
+      },
+      onChange(current) {
+        this.index = current
+      }
+    },
+    components: {
+      Seller,
+      Ratings,
+      Goods
+    },
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+
+  .tab
+    display: flex
+    flex-direction: column
+    height: 100%
+    >>> .cube-tab
+      padding: 10px 0
+    .slide-wrapper
+      flex: 1
+      overflow: hidden
+</style>
